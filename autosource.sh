0cec7abf7624a13372ee70742a9252b6aeeebb12
Samuele Sabella <sabella@ntop.org>
2019-10-25 09:33:40 +0200

    fs commit :tada:

---
+++ autosource.sh
@@ -0,0 +1,32 @@
+# >>> autosource <<< #
+complete -W $(ls) dothis
+
+function deactivate_check {
+  if [ ! -z "${VIRTUAL_ENV}" ]; then
+    deactivate
+  fi
+}
+
+function autosource { 
+  # Navigating parent directory
+  local ldir=$(pwd)
+  while [ ${ldir} != "/" ]; do
+    if [ -d "${ldir}/venv" ]; then
+      deactivate_check
+      source "${ldir}/venv/bin/activate"
+      return
+    fi
+    ldir=$(dirname ${ldir})
+  done
+  deactivate_check
+}
+
+function lazycd {
+  cd $1
+  autosource
+}
+
+LAZYCD="v0.1"
+alias cd=lazycd
+autosource
+
---
+++ installer.sh
@@ -0,0 +1,15 @@
+function autosource_install {
+  local profile=~/.bash_profile
+  if [ "$OSTYPE" == "linux-gnu" ]; then
+    profile=~/.bashrc
+  fi
+
+  if ! grep -q ">>> autosource <<<" $profile ; then
+    echo "\\n$(cat autosource.sh)" >> ${profile}
+    echo "autosource: installed"
+  else 
+    echo "autosource: already installed"
+  fi
+}
+
+autosource_install
